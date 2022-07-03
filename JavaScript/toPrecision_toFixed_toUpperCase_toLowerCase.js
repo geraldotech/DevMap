a6e74aca9ac1ff6427030a9d4ffdc66f9dc39352
geraldo.tech <geraldo.filho92@hotmail.com>
2022-07-03 18:16:03 -0300

    new

---
+++ JavaScript/toPrecision_toFixed_toUpperCase_toLowerCase.js
@@ -0,0 +1,9 @@
+//toFixed depois da virgula
+console.log(Math.PI.toFixed(2)); // 3.14
+//
+console.log(Math.PI.toPrecision(2)); //3.1
+
+let firtName = "Geraldo";
+console.log(firtName.toUpperCase()); //GERALDO
+
+console.log(firtName.toLowerCase()); //geraldo
