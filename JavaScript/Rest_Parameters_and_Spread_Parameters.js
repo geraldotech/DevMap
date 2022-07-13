163509ff909d1b0dd7ef6e80e6602eff6dd23077
geraldo.tech <geraldo.filho92@hotmail.com>
2022-07-12 22:53:31 -0300

    doc

--- JavaScript/Rest_Parameters_and_Spread_Parameters.js
+++ JavaScript/Rest_Parameters_and_Spread_Parameters.js
@@ -1,34 +1,3 @@
-//1
-const app = document.createElement("div");
-app.setAttribute("id","app");
-app.textContent = "Hi mon!";
-document.body?.appendChild(app);
-
-//2 type annotation é quando definir o dado manualmente
-//type inference é quando o TypeScript define de forma automatica
-
-//3 number, string and boolean
-
-//4 
-function somar(a:number, b:number){
-    console.log(`${a + b}`)
-}
-somar(2,4)
-
-//5
-console.log(Math.PI.toFixed(2)); //depois da virgula
-console.log(Math.PI.toPrecision(2)) //todos os numbers
-
-//6
-const arr:number [] = [1,2,3,4];
-const arr2:Array <number> = [5,6,7,8,9,10];
-console.log(...arr)
-const arr3:string [] = ["a","b","c"];
-
-//see more https://www.youtube.com/watch?v=j3bXlt3NOZA
-
-// https://dev.to/sagar/three-dots---in-javascript-26ci
-
 //Spread Operators ...
 const featured = ['Deep Dish', 'Pepperoni', 'Hawaiian'];
 const specialty = ['Meatzza', 'Spicy Mama', 'Margherita'];
@@ -37,6 +6,13 @@ const pizzas = [...featured,'vegan',...specialty];
 
 console.log(pizzas); 
 
+
+//
+
+const arr:number [] = [1,2,3,4];
+const arr2:Array <number> = [5,6,7,8,9,10];
+console.log(...arr)
+const arr3:string [] = ["a","b","c"];
 
 //Rest Parameters
 // allow you to pass in an indefinite number of arguments into a function.
@@ -80,3 +56,6 @@ console.log(res2) // 1,2 20+30 = 50
 
 
 
+//see more https://www.youtube.com/watch?v=j3bXlt3NOZA
+
+// https://dev.to/sagar/three-dots---in-javascript-26ci
