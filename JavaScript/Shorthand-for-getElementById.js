e756b04e8dd8d6d7571f29fcd8c554ef4ae437d5
geraldo.tech <geraldo.filho92@hotmail.com>
2022-07-30 12:26:53 -0300

    update

--- JavaScript/Shorthand-for-getElementById.js
+++
@@ -1,9 +0,0 @@
-//Shorhand for getElementById
-
-//function
-const get = function(id){ return document.getElementById(id) };
-get("root").textContent = 'Hello World!';
-
-//arrow function
-const get2 = (a) => {return document.getElementById(a)};
-get2("root");
