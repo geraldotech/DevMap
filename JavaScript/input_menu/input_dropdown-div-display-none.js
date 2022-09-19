ab4611b02545ca9c13f0e0812aaac739b6227c3b
geraldo.tech <geraldo.filho92@hotmail.com>
2022-09-19 20:41:07 -0300

    :pencil: :books: :zap: DOC

--- JavaScript/input_menu/input_dropdown-div-display-none.js
+++ JavaScript/input_menu/input_dropdown-div-display-none.js
@@ -1,112 +1,117 @@
+/*
+version 2.0
+https://gmapdev.netlify.app/projects/consulta-limites/index.html
+*/
+
 //CSS
-<style>
-   body {
-    display: block;
-    margin: 20px;
-    margin-left: auto;
-    margin-right: auto;
-    height: 100vh;
-    background-color: #f1f1f1;
-    padding: 5px;
-  }
-  /*pode usar qualquer nome e.g: b.k*/
+body {   
+  margin:  0 100px;
+  height: 100vh;
+  background-color: #f1f1f1;
+  padding: 5px;
+}
+/*pode usar qualquer nome e.g: b.k*/
 div {
-   display: none; 
-   flex-wrap: wrap;
-   flex-direction: row;
-   justify-content: center;
-   align-items: center;
-   height: auto;
-   margin-left: auto;
-   margin-right: auto;
-  
+ display: none; 
+
+ align-items: center;
+ height: auto;
+ margin-left: auto;
+ margin-right: auto;
+
 }    
-.imagem {
- width: 20%;
+
+main.container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+}
+img.imagem {
+width: 50%;
+max-width: 50%;
 }
 .limite {
- color: green;
- font-weight: bold;
+color: green;
+font-weight: bold;
 }
 h1 {
- font-size: 30px;
+font-size: 30px;
 }
 select {
-       margin-bottom: 10px;
-       margin-top: 10px;
-       outline: 0;
-       background: #040806;
-       color: #fff;
-       border: 1px solid crimson;
-       padding: 4px;
-       border-radius: 9px;
-     }
-     
+     margin-bottom: 10px;
+     margin-top: 10px;
+     outline: 0;
+     background: #040806;
+     color: #fff;
+     border: 1px solid crimson;
+     padding: 4px;
+     border-radius: 9px;
+   }
+   
 th, td {
- background-color: lightgray;
- border: 1px solid black;
+background-color: lightgray;
+border: 1px solid black;
 }
 table {
- width: 50%;
- text-align: center;
- font-family: arial, sans-serif;
- border-collapse: collapse;
+width: 50%;
+text-align: center;
+font-family: arial, sans-serif;
+border-collapse: collapse;
 }
 
 footer {
-    display: inline;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    margin-top: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
 }
 ul li {
-    list-style: none;
+  list-style: none;
 }
   </style>
 
 //html
-    <h1>CONSULTA CARTÕES</h1>
-       
-        <select name="option" id="options">
-        <option value="1">Selecionar: </option>
-        <option value="2">Santander</option>
-        <option value="3">Trigg</option>
-        <option value="4">Banco do Brasil</option>
-        <option value="5">test </option>
-        </select>
+<main class="container">
+<section>
+<h1>input_menu display_div</h1>       
+<select name="option" id="options">
+<option value="1">Selecionar: </option>
+<option value="2">A</option>
+<option value="3">B</option>
+</select>
 
-        <div id="1" style="display: block"></div>
-        <div id="2"><img src="https://www.santander.com.br/sites/WPC_CMS/imagem/21-09-08_194432_santander-logo.png" class="imagem"/>
-        <br>Santander SX: 8600 
-        <br><p class="limite">Limite: R$ 5.000</p>Disponivel: R$ 1,500<br></div>
-        <div id="3">
-        <img src="http://cdn-content.geraldo.tech/img/cartao-de-credito-trigg-coringa.png" class="imagem" />
-        <table>
-        <tr>
-          <th>FINAL</th>
-          <th>Limite</th>
-          <th>Disponível</th>
-        </tr>
-        <tr>
-          <td>5633</td>
-          <td>R$ 10,000</td>
-          <td>R$ 3,000</td>
-        </tr>
-        </table>
-        </div>
-        <div id="4"><img src="https://assets.weforum.org/organization/image/ilLBe16nTh4mtkP7ClVjA4TkEfcY0TCv65nq5QvUUSE.jpeg" class="imagem"/></div>
-        <div id="5">Test 5</div>
+<div id="1" style="display: block"></div>
+<div id="2">
+     <img src="https://www.santander.com.br/sites/WPC_CMS/imagem/21-09-08_194432_santander-logo.png" class="imagem"/>
+<br>Santander SX: 8600 
+<br><p class="limite">Limite: R$ 5.000</p>Disponivel: R$ 1,500<br></div>
+<div id="3">
+<table>
+<tr>
+<th>FINAL</th>
+<th>Limite</th>
+<th>Disponível</th>
+</tr>
+<tr>
+<td>5633</td>
+<td>R$ 10,000</td>
+<td>R$ 3,000</td>
+</tr>
+</table>
+</div>  
 
 
-        <footer>
-            <ul>
-                <li>Geraldo Filho - <ano id="ano"> </ano> </li>
-            </ul>
-        </footer>
+<footer>
+ <ul>
+     <li>Geraldo Filho - <ano id="ano"> </ano> </li>
+ </ul>
+</footer>
+</section>
+</main>
 
 //JS
-<script>
 document.getElementById("options").onchange = function () {
     var i = 1;
     var myDiv = document.getElementById(i);
@@ -120,4 +125,7 @@ document.getElementById("options").onchange = function () {
 const d = new Date();
 let ano = d.getFullYear();
 document.getElementById("ano").innerHTML = ano;
-</script>
+//inline
+// document.getElementById("ano").innerHTML = new Date().getFullYear();
+    </script>
+    
--- JavaScript/input_menu/input_radio-button-IDvs-Onclick.js
+++ JavaScript/input_menu/input_radio-button-IDvs-Onclick.js
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>input radio button onClick</title>
     <style>
-       
+
     </style>
 </head>
 <body>
+
     <h1><label for="plan">1 - No ID</label><br></h1>
-    <input type="radio" name="plan" onclick="geta()">10GB<br>
-    <input type="radio" name="plan" onclick="getb()">50GB<br>
+    <input type="radio" name="plan" id="10" onclick="geta()">
+    <label for="10">10GB</label>
+    <input type="radio" name="plan" id="50" onclick="getb()">
+    <label for="50">50GB</label>
     <g></g>
     <span id="res"></span>
 <hr>
 <h1><label for="banks">2 - Com ID</label><br></h1>
-    <input type="radio" name="bank" id="sx">Santander<br>
-    <input type="radio" name="bank" id="bb">Banco do Brasil<br>
-    <input type="button" value="clean" id="refresh"><br>
+    <input type="radio" name="bank" id="sx">
+    <label for="sx">Santander</label>
+    <input type="radio" name="bank" id="bb">
+    <label for="bb">Banco do Brasil</label>
+    <input type="button" value="clean" id="cleanHtml"><br>
 <b></b>
 <g></g>
-
-</body>
-<script>
-//2 - Com ID
+    
+   <script>
+//1 - Com ID
 const body = document.querySelector('body');
 var b = document.querySelector('b');
 document.getElementById('sx').onclick = function(){
@@ -38,19 +41,21 @@ document.getElementById('bb').onclick = function(){
     body.style.backgroundColor = 'orange';
 }
 
-document.getElementById('refresh').onclick = function(){
-    location.reload();
+document.getElementById('cleanHtml').onclick = function(){
+    //location.reload();
+    // do not refresh page, apenas limpa os styles
+    body.style.background = "";
 }
 
-//1 - No ID
+//2 - No ID
 var p = document.querySelector('g');
-//var p = document.getElementById('res');
 function geta(){
     p.innerHTML = '<div style="background-color:dodgerblue;width:300px;height:100px;color:white">10 gigas</div>';
-}
+};
 function getb(){
     p.innerHTML = '<div style="background-color:purple;width:300px;height:100px;color:white">50 gigas</div>';
-}
-</script>
+};
 
+   </script>
+</body>
 </html>
