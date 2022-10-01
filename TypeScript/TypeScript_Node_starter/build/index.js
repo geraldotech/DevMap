bf38b441403aecd91322f2a8c36aae8a5b8397e0
geraldo.tech <geraldo.filho92@hotmail.com>
2022-10-01 08:34:45 -0300

    :pencil: :zap: :comet:

--- TypeScript/TypeScript_Node_starter/build/index.js
+++ TypeScript/TypeScript_Node_starter/build/index.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log('Hello world!');
+var age = 30;
+var name = "Mark";
+console.log("My name is ".concat(name, ", I am ").concat(age, " years old!"));
+var Numbers = [1, 2, 3, 4];
+console.log(Numbers.reduce(function (total, val) { return total += val; }));
