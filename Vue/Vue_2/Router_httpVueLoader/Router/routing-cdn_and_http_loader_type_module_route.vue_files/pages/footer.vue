f121f7d9bda35be2668dc0c3d25cad2afaba81f3
geraldo.tech <geraldo.filho92@hotmail.com>
2022-11-28 20:13:28 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Vue_2/Router_httpVueLoader/Router/routing-cdn_and_http_loader_type_module_route.vue_files/pages/footer.vue
+++ Vue/Vue_2/Router_httpVueLoader/Router/routing-cdn_and_http_loader_type_module_route.vue_files/pages/footer.vue
@@ -1,14 +1,52 @@
 <template>
   <footer>
-    <p>by gmapdev</p>
-    <p>footer.vue</p>
+    <div>
+      <p>by gmapdev - footer.vue - {{ ab }}</p>
+    </div>
+    <!-- <div id="appz">
+      <nav>
+        <ul>
+          <router-link class="nav-link" to="/"> Home </router-link>
+        </ul>
+      </nav>
+      <router-view></router-view>
+    </div> -->
   </footer>
 </template>
 
 <style scoped>
 footer {
+  padding-top: 15px;
   background: rgb(63, 39, 31);
   height: 50px;
   text-align: center;
 }
 </style>>
+<script>
+module.exports = {
+  data: function () {
+    return {
+      ab: "Segunda",
+    };
+  },
+};
+
+/* var routes = [
+  { path: "/", component: Home },
+  { path: "/download", component: httpVueLoader("./pages/download.vue") },
+  { path: "/sobre", component: httpVueLoader("./pages/sobre.vue") },
+];
+
+var router = new VueRouter({
+  routes: routes,
+  mode: "history",
+  //base: "/#",
+  base: "/",
+});
+
+var app = new Vue({
+  el: "#app",
+  router: router,
+}); */
+</script>
+
