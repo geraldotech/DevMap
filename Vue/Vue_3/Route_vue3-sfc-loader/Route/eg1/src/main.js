98746ed9a5f6741236b6343e874f47563c9e9cc2
geraldo.tech <geraldo.filho92@hotmail.com>
2022-11-28 19:59:16 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Vue_3/Route_vue3-sfc-loader/Route/eg1/src/main.js
+++ Vue/Vue_3/Route_vue3-sfc-loader/Route/eg1/src/main.js
@@ -10,16 +10,18 @@ const Home = { template: `<h1>Home</h1>` };
 const About = { template: `${About2}` };
 const Portfolio = { template: `<h1>Contenuto Portfolio</h1>` };
 const Contatti = { template: `<h1>Contenuto Contatti</h1>` };
+const PathNotFound = { template: `<h1>404 Error!</h1>` };
 
 const routes = [
   { path: "/", component: Home },
   { path: "/about", component: About2 },
   { path: "/portfolio", component: Portfolio },
+  { path: "/:pathMatch(.*)*", component: PathNotFound },
 ];
 
 const router = VueRouter.createRouter({
-  //history: VueRouter.createWebHashHistory(location.pathname),
-  history: VueRouter.createWebHistory(), //removes #!
+  history: VueRouter.createWebHashHistory(location.pathname), //support 404 error
+  //history: VueRouter.createWebHistory(location.pathname), //removes #! but don't support 404
   routes,
 });
 
--- Vue/Vue_3/Route_vue3-sfc-loader/Route/eg2/src/main2.js
+++ Vue/Vue_3/Route_vue3-sfc-loader/Route/eg2/src/main2.js
@@ -10,19 +10,26 @@ const About = {
 };
 const Port = { template: "<div>route Port</div>" };
 const Pets = { template: "<div>route pets</div>" };
+const PathNotFound = { template: `<h1>404 Error!</h1>` };
 
 const routes = [
   { path: "/", component: Home },
   { path: "/route1", component: About },
   { path: "/route2", component: Port },
   { path: "/route3", component: PetsJ }, //pega a const de pets.js
+  { path: "/:pathMatch(.*)*", component: PathNotFound },
 ];
 
 const router = VueRouter.createRouter({
   //importante para criar o: #/
-  history: VueRouter.createWebHashsHistory(),
+  //history: VueRouter.createWebHashHistory(location.pathname), //
+  /*
+   ***removes #! but don't support direct call page see https://v3.router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
+    history: VueRouter.createWebHistory(location.pathname),
+    ler mais sobre createWebHash... https://router.vuejs.org/api/#createwebhashhistory
+   */
+  history: VueRouter.createWebHashHistory("/#app/"),
   routes,
-  base: "/page/",
 });
 
 app.use(router);
