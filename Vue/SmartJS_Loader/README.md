9cb011cc6d117b2437e22b2a865b059846a25ffe
geraldo.tech <geraldo.filho92@hotmail.com>
2023-01-26 21:15:49 -0300

    :pencil: :books: :zap: DOCS

---
+++ Vue/SmartJS_Loader/README.md
@@ -0,0 +1,20 @@
+# Smart JS Loader
+
+> Lets html more clear, add script in header with this plugin
+
+> 👉 Multiple scripts on DOM Loader
+> 👉 Local and remote Scripts
+> 👉 Require window.onload call the functions after window loading
+
+```js
+SmartLoader("script.js");
+
+window.onload = function () {
+  //code goes here
+};
+```
+
+[https://smartjs-loader.netlify.app/](https://smartjs-loader.netlify.app/)
+
+[https://htmldom.dev/load-a-javascript-file-dynamically/](https://htmldom.dev/load-a-javascript-file-dynamically/)
+[https://www.kirupa.com/html5/loading_script_files_dynamically.htm](https://www.kirupa.com/html5/loading_script_files_dynamically.htm)
---
+++ Vue/SmartJS_Loader/src/main.js
@@ -0,0 +1,33 @@
+import SmartLoader from "./smartLoader.js";
+
+SmartLoader("https://cdn.jsdelivr.net/npm/vue@2.7.13/dist/vue.js");
+SmartLoader("https://unpkg.com/vue-router@3.0.2/dist/vue-router.js");
+SmartLoader("https://unpkg.com/http-vue-loader");
+SmartLoader("./src/test.js");
+
+window.onload = function () {
+  const Home = {
+    template: `<p>Home Page</p>`,
+  };
+  const About = { template: `<p>About Page</p>` };
+
+  const routes = [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+  ];
+
+  const rota = new VueRouter({
+    routes: routes,
+  });
+  const app = new Vue({
+    el: "#app",
+    router: rota,
+    data: {
+      welcome: "welcome",
+    },
+    components: {
+      App: httpVueLoader("./src/app.vue"),
+    },
+    template: `<App/>`,
+  });
+};
