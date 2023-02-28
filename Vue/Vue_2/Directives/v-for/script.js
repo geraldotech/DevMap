f749c80e19d07b3c5ba5d83aae75f1599119d3e5
geraldo.tech <geraldo.filho92@hotmail.com>
2023-02-27 21:45:35 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Vue_2/Directives/v-for/script.js
+++ Vue/Vue_2/Directives/v-for/script.js
@@ -1,11 +1,25 @@
 const app = new Vue({
-    el: "#app",
-    data: {
-        list:[
-            {id:1, name: 'Geraldo'},
-            {id:2, name: 'Felipe'},
-            {id:3, name: 'Milly'},
-            {id:4, name: 'Isa'},
-        ]
-    }
+  el: "#app",
+  data: {
+    list: [
+      { id: 1, name: "Geraldo" },
+      { id: 2, name: "Felipe" },
+      { id: 3, name: "Milly" },
+      { id: 4, name: "Isa" },
+    ],
+    categories: {
+      android: 2,
+      zeta: 1,
+      uncategorized: 1,
+      youtube: 1,
+      upload: 1,
+      downloads: 2,
+    },
+    numbers: [1, 2, 3, 4, 5],
+  },
+  computed: {
+    evenNum() {
+      return this.numbers.filter((val) => val % 2 === 0);
+    },
+  },
 });
