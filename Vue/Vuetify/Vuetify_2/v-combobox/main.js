899723aa90edd2c5d4bceb4ffe084677f5c44cc2
geraldo.tech <geraldo.filho92@hotmail.com>
2023-03-10 20:45:00 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Vuetify/Vuetify_2/v-combobox/main.js
+++ Vue/Vuetify/Vuetify_2/v-combobox/main.js
@@ -6,8 +6,11 @@ new Vue({
   data: {
     combo: [],
     operadoras: [
-      { id: 1, name: "Vivo", plano: "12GB" },
-      { id: 2, name: "TIM", plano: "10GB" },
+      { id: 1, name: "Vivo", plano: "12GB", color: "purple" },
+      { id: 2, name: "TIM", plano: "10GB", color: "blue" },
+      { id: 3, name: "Claro", plano: "13GB", color: "red" },
     ],
+    depende: false,
+    vivosub: "",
   },
 });
