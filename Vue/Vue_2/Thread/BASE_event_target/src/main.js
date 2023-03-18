a775e3525a09154c8985cabf8d7357fdd2d49829
geraldo.tech <geraldo.filho92@hotmail.com>
2023-03-17 21:20:39 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Vue_2/Thread/BASE_event_target/src/main.js
+++ Vue/Vue_2/Thread/BASE_event_target/src/main.js
@@ -8,10 +8,15 @@ const vm = new Vue({
   },
   methods: {
     myfun: function ($event) {
+      // get id
       console.log(
         $event.target.options[$event.target.options.selectedIndex].dataset.id
       );
-      console.warn($event);
+      // get Index by gmapx.com
+      console.warn($event.target.options.selectedIndex);
+      // como visto em https://github.com/geraldotech/DevMap/blob/main/JavaScript/select/README_from_scratch.js
+      console.warn($event.target.selectedOptions[0].dataset.id);
+      console.warn($event.target.selectedOptions[0].getAttribute("data"));
     },
   },
 });
