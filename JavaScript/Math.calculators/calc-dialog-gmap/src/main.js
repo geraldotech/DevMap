f0819f1014116a18f4e53bcc8070833446c5f053
geraldo.tech <geraldo.filho92@hotmail.com>
2023-04-22 14:06:05 -0300

    Fixed clean old values && added toFixed()

--- JavaScript/Math.calculators/calc-dialog-gmap/src/main.js
+++ JavaScript/Math.calculators/calc-dialog-gmap/src/main.js
@@ -26,20 +26,29 @@ button.innerText = `Computar`;
 dialog.appendChild(button);
 
 const Calc = document.querySelector("dialog button[name='ok']");
-const todosNum = Array.from(document.querySelectorAll("input[type='number']"));
+const inputsNum = document.querySelectorAll("input[type='number']");
+const todosNum = Array.from(inputsNum);
 
 Calc.onclick = function () {
+  /*  // Requer no mínimo uma nota valida
+  if (todosNum.every((val) => val.value.length != 0)) {
+    return alert("Verificar os campos!");
+  } */
+
   let filval = todosNum.map((val) => +val.value);
   let soma = filval.reduce((total, val) => (total += val));
   let media = soma / 3;
-  console.log(filval);
+  console.log(`filval`, filval);
   document.getElementById("res").innerHTML = `<p>Notas: ${filval.map(
     (a) => `[${a}]`
-  )}</p> <p>Soma: ${soma} </p> <p>Media: ${media}</p> 
+  )}</p> <p>Soma: ${soma} </p> <p>Media: ${media.toFixed(2)}</p> 
         <p>Situação: ${media >= 6 ? "Aprovado" : "Reprovado"}</p>`;
   dialog.close(); //para fechar o dialog after click
 };
 //clean
 document.querySelector("button[name='clear']").onclick = function () {
   document.getElementById("res").innerHTML = "";
+  //Clean old values
+  //console.warn(inputsNum[0].value);
+  inputsNum.forEach((val) => (val.value = ""));
 };
