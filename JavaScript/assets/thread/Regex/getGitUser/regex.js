e120ee8503d461358d719558c230b4b6ab46ac15
geraldo.tech <geraldo.filho92@hotmail.com>
2023-06-10 20:29:19 -0300

    :pencil: :books: :zap: DOCS

--- JavaScript/assets/thread/Regex/getGitUser/regex.js
+++ JavaScript/assets/thread/Regex/getGitUser/regex.js
@@ -8,18 +8,35 @@ function getUserNamefromURL(url) {
   const search = url.match(myregex);
   console.warn(search);
   const userName = search[1];
+  const UserNameOnly = userName.charAt(1) + userName.substr(2);
+  console.warn(UserNameOnly);
 
+  //👉 exceto
   function exceto(arr, keys) {
     return arr.filter((val, ind) => {
       return typeof keys === `number` ? ind !== keys : keys.indexOf(ind) === -1;
     });
   }
+  const afterExeto = exceto(search, [0, 2, 3, 4].join(" "));
+  console.log(`afterExeto:`, afterExeto);
+  console.log(`afterExeto:`, afterExeto[0]);
+  console.log(`afterExeto:`, afterExeto[1]);
+  console.log(
+    `afterExeto char and sub:`,
+    afterExeto[1].charAt(1) + afterExeto[1].substr(2)
+  );
+  console.log(`afterExeto:`, afterExeto[2]);
 
-  console.log(`exceto`, exceto(search, [0, 2, 3, 4]));
-
-  const UserNameOnly = userName.charAt(1) + userName.substr(2);
-  console.warn(UserNameOnly);
-
-  userName.innerHTML = UserNameOnly;
   document.querySelector("#userName").innerHTML = UserNameOnly;
+  document.querySelector("#userDir").innerHTML = exceto(search, [0, 2, 3, 4]);
 }
+
+function par() {
+  let x = "";
+  for (let i = 0; i < 10; i++) {
+    x += i;
+  }
+  return x;
+}
+
+document.getElementById("par").innerHTML = par();
