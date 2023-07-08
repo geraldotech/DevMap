978ad14c0a3f16a2498870f39241131832c949e3
geraldo.tech <geraldo.filho92@hotmail.com>
2023-07-08 09:27:59 -0300

    :pencil: :books: :zap: DOCS

--- PHP/JSON_php_file.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myObj = new stdClass();
-$myObj->name = "Geraldo";
-$myObj->age = 30;
-$myObj->city = "New York";
-$myObj->city2 = "Maceio";
-$myJSON = json_encode($myObj);
-echo $myJSON;
-?>
--- PHP/somar.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST['btn'])){
-	$x = $_POST['n1'];
-	$y = $_POST['n2'];
-	$z = $x + $y;
-	echo "soma = ".$z;
-}
-?>
