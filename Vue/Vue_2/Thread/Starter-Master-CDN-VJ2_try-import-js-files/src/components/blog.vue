f7a28685282bb6ad5d439c01cfd08ff711d2d340
geraldo.tech <geraldo.filho92@hotmail.com>
2023-07-15 18:06:31 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Vue_2/Thread/Starter-Master-CDN-VJ2_try-import-js-files/src/components/blog.vue
+++ Vue/Vue_2/Thread/Starter-Master-CDN-VJ2_try-import-js-files/src/components/blog.vue
@@ -1,20 +1,11 @@
 <template>
   <div>
     <h1>blog.vue</h1>
-
-    <p>{{ Compois }}</p>
     <component :is="Compois.map"></component>
+    <component :is="Compois.rtx"></component>
   </div>
 </template>
 <script>
-/* import { defineComponent } from "@vue/composition-api";
-
-export default defineComponent({
-  setup() {},
-}); */
-//import test from "../src/components/test.js";
-//httpVueLoader.register(Mytest, "../src/components/test2.vue");
-
 module.exports = {
   mounted() {
     console.log(`from`, Posts);
@@ -25,6 +16,7 @@ module.exports = {
   },
   data() {
     return {
+      //Compois rebe os valores de Posts
       Compois: Posts,
     };
   },
--- Vue/Vue_2/Thread/Starter-Master-CDN-VJ2_try-import-js-files/src/components/test2.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="rotas">
-    <h2>test2.vue</h2>
-  </div>
-</template>
-<script>
-module.exports = {};
-</script>
