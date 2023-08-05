2be0a9f67e78a4fa5ad695b80dc784836a849423
geraldo.tech.dell <geraldo.filho92@hotmail.com>
2023-08-05 09:12:18 -0300

    :pencil: :books: :zap: DOCS

--- JavaScript/assets/thread/Web-Components/main.js
+++ JavaScript/assets/thread/Web-Components/main.js
@@ -5,16 +5,22 @@ class BlogPost extends HTMLElement {
     this.shadowRoot.append(this.connectedCallback());
   }
   connectedCallback() {
-    const template = document.createElement("template");
-    template.innerHTML = `
-   <style> 
-       h1{
-           color: dodgerblue;
-       }
-   </style>
-   <h1>Hello using connectedCallback()</h1>
-   `;
-    return template.content;
+    this.shadowRoot.append(this.test(), this.article());
+  }
+  test() {
+    console.log(`styles`);
+    const styles = document.createElement("style");
+    styles.textContent = `  
+    div{
+        color: red;
+    }
+    `;
+    return styles;
+  }
+  article() {
+    const div = document.createElement("div");
+    div.textContent = `Hello`;
+    return div;
   }
 }
 
