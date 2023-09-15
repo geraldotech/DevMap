859e2a71605be53aae663e34382422923c956064
gmap-nitro <geraldo.filho92@hotmail.com>
2023-09-15 19:10:24 -0300

    :pencil: :books: :zap: DOCS

---
+++ Vue/Vue_2/Vue-extend/README.md
@@ -0,0 +1,3 @@
+# Vue.extend
+
+also creates an instance of Vue Extends funcionando similar ao `new Vue` com a própria ordem de $mount, porém também funcionam como Components, sendo comentário ao declrar components o Vue under the hood chama o extends.
--- Vue/Vue_2/Vue-extend/src/main.js
+++ Vue/Vue_2/Vue-extend/src/main.js
@@ -3,12 +3,7 @@ https://v2.vuejs.org/v2/api/#Global-Config
 */
 
 //Suppress all Vue logs and warnings.
-Vue.config.silent = true;
-
-/* 
-Vue.extend also creates an instance of Vue
-Extends funcionando similar ao `new Vue` com a própria ordem de $mount, porém também funcionam como Components, sendo comentário ao declrar components o Vue under the hood chama o extends.
-*/
+Vue.config.silent = true
 
 /* definindo um extend com $mount */
 const Perfil = Vue.extend({
@@ -16,10 +11,10 @@ const Perfil = Vue.extend({
   data: function () {
     return {
       hello: `Ola from Vue extend one`,
-    };
+    }
   },
-});
-new Perfil().$mount("#al");
+})
+new Perfil().$mount('#al')
 
 /* definindo outro vue.extend com $mount e será usado como Component e aproveitando esta chamando um component que foi definido acima em Vue.extend */
 const RTX = Vue.extend({
@@ -27,29 +22,29 @@ const RTX = Vue.extend({
   data: () => {
     return {
       hello: `Ola from Vue extend RTX two`,
-    };
+    }
   },
   components: {
     MyPerfil: Perfil, // Extend chamada outro Extend como component
   },
-});
-new RTX().$mount("#rtx");
+})
+new RTX().$mount('#rtx')
 
 // também podemos $mount usando `el`
 const elmounta = Vue.extend({
   data() {
     return {
-      msn: "Hello from #elmount Adicional:",
-      adicional: "99",
-    };
+      msn: 'Hello from #elmount Adicional:',
+      adicional: '99',
+    }
   },
   template: `<p>{{msn}} {{adicional}}</p>`, // esse object nao existe no data porem vamos preencher a informacao na montagem, onde overwrite o data() se houver
-});
+})
 /* new elmounta({
   el: "#elmount",
   data: { adicional: "InfoDireta no new" },
 }); */
-new elmounta({ data: { adicioxnal: 1233 } }).$mount("#elmount");
+new elmounta({ data: { adicioxnal: 1233 } }).$mount('#elmount')
 
 //eg extender a extender
 //Nesse nodo extendes vou pegar os dados do `elmounta`
@@ -58,16 +53,16 @@ const elmountAndSo = elmounta.extend({
   data: () => {
     return {
       songs: 1000,
-      artist: "Taylor",
-      adicional: "100", // obj definido do extends[elmounta]
-    };
+      artist: 'Taylor',
+      adicional: '100', // obj definido do extends[elmounta]
+    }
   },
   template: `<div>Hello from extender a extender:
   <p>{{adicional}}</p>
   <p>{{msn}}</p> como essa obj existe do outra extends a informacao foi herdada, porem basta definir novo valor no data()
   </div>`,
-});
-new elmountAndSo({ el: "#elmountAndSongs" });
+})
+new elmountAndSo({ el: '#elmountAndSongs' })
 
 /* 
 comment on stackoverflow
@@ -76,13 +71,13 @@ I think the confusion is because extend & component are closely connected. To cr
 //thanks: https://stackoverflow.com/questions/40719200/what-is-vue-extend-for
 //
 const vm = new Vue({
-  el: "#app",
+  el: '#app',
   data: {},
   template: `<p>Hello from default instance Vue  {{hi}} <testExtende></testExtende></p>`,
   components: {
     testExtende: RTX, //call the vue.extend here
   },
-});
+})
 
 //dark theme
-document.documentElement.style = `color-scheme:dark`;
+document.documentElement.style = `color-scheme:dark`
