1473e47a7cb64091549ffcc2a17a489671b20d81
gmap-nitro <geraldo.filho92@hotmail.com>
2023-11-01 21:17:45 -0300

    :pencil: :books: :zap: DOCS

--- JavaScript/Clipboard/copy-snippet/src/main.js
+++ JavaScript/Clipboard/copy-snippet/src/main.js
@@ -1,46 +1,48 @@
-const code = document.querySelectorAll(".clipboard code");
-const btnCopy = document.querySelectorAll("#btncopy");
-const div = document.querySelectorAll(".clipboard");
+const code = document.querySelectorAll('.clipboard code')
+const btnCopy = document.querySelectorAll('#btncopy')
+const div = document.querySelectorAll('.clipboard')
 
 btnCopy.forEach((val) => {
-  val.addEventListener("click", handlerBoard);
-});
+  val.addEventListener('click', handlerBoard)
+})
 
 function handlerBoard(e) {
-  console.log("target", e.target.firstChild); // button
-  console.log("currentTarget", e.currentTarget.parentElement); // div.clipboard
-  console.log("this", this);
+  console.log('target', e.target.firstChild) // button
+  console.log('currentTarget', e.currentTarget.parentElement) // div.clipboard
+  console.log('this', this)
 
   // firstChild = current Text click
   const codeAtual =
-    e.currentTarget.parentElement.querySelector("code").textContent;
-  const btnAtual = e.target;
+    e.currentTarget.parentElement.querySelector('code').textContent
 
   // passar o valor do currentTarget selecionando o valor de <code>
   navigator.clipboard.writeText(codeAtual).then(() => {
     //btnAtual.textContent = "Copied"; //precisa ser o button atual
 
+    /* want change text after click */
     /*  setInterval(function () {
-      btnAtual.textContent = "Copy";
-    }, 1000); */
+      btnAtual.textContent = 'Copy'
+    }, 1000) */
 
     /*
     loop iterate over all buttons and check who is current contains e.target[atual]
     */
 
     for (const current of btnCopy) {
-      // loop checkup elementp contains o eventoAtual
-      let atual = current.contains(e.target);
-      console.log(current.contains(this.target));
+      // loop checkout element contains o eventoAtual
+      let currentEvent = current.contains(e.target)
+      //console.log(current.contains(this.target))
       // quem for true recebe
-      /*     if (atual) {
+      /*     if (currentEvent) {
         current.textContent = "Copied";
       } else {
         // who false
         current.textContent = "Copy";
       } */
       // Ternay version
-      atual ? (current.textContent = "Copied") : (current.textContent = "Copy");
+      currentEvent
+        ? (current.textContent = 'Copied')
+        : (current.textContent = 'Copy')
     }
-  });
+  })
 }
