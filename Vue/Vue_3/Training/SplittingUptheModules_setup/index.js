ab940a23fcf5f01955147c0b0ea2efbbf6582137
gmap-nitro <geraldo.filho92@hotmail.com>
2024-03-03 16:11:45 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Vue_3/Training/SplittingUptheModules_setup/index.js
+++ Vue/Vue_3/Training/SplittingUptheModules_setup/index.js
@@ -2,9 +2,14 @@ const { createApp, ref, computed, watch } = Vue;
 
 const app = Vue.createApp({
   setup(){
-    const message = ref('Hello Vue!')
+    const message = ref('Hello')
     return {
       message
     }
+  },
+  data(){
+   return {
+    foo: 'Vue !'
+   }
   }
 }).mount("#app")
