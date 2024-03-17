c4f35c2a1f8e128d3fd09dbd0fc433eaeb810e87
gmap-nitro <geraldo.filho92@hotmail.com>
2024-03-17 19:27:13 -0300

    :pencil: :books: :zap: DOCS

--- Vue/Libs/PrimeVue/Vue2BASE-umd_esm/src/main.js
+++ Vue/Libs/PrimeVue/Vue2BASE-umd_esm/src/main.js
@@ -1,7 +1,7 @@
 /* import Vue from "https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.esm.browser.js"; */
 
 const vm = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     isa: 'Isabella',
     isChecked: false,
@@ -12,46 +12,69 @@ const vm = new Vue({
     selectedCountry: null,
     isInvalid: '',
     inputswitch: false,
+    errorToast: false,
+    displayBasic: false,
+    displayBasic2: false,
+    visibleLeft: false,
     citiesbr: [
-			{id: 1, name: 'New York', code: 'NY'},
-			{id: 2, name: 'Rome', code: 'RM'},
-			{id: 3, name: 'London', code: 'LDN'},
-			{id: 4, name: 'Istanbul', code: 'IST'},
-			{id: 5, name: 'Paris', code: 'PRS'}
-		],
+      { id: 1, name: 'New York', code: 'NY' },
+      { id: 2, name: 'Rome', code: 'RM' },
+      { id: 3, name: 'London', code: 'LDN' },
+      { id: 4, name: 'Istanbul', code: 'IST' },
+      { id: 5, name: 'Paris', code: 'PRS' },
+    ],
     countries: [
-      {name: 'Australia', code: 'AU'},
-      {name: 'Brazil', code: 'BR'},
-      {name: 'China', code: 'CN'},
-      {name: 'Egypt', code: 'EG'},
-      {name: 'France', code: 'FR'},
-      {name: 'Germany', code: 'DE'},
-      {name: 'India', code: 'IN'},
-      {name: 'Japan', code: 'JP'},
-      {name: 'Spain', code: 'ES'},
-      {name: 'United States', code: 'US'},
-  ]
+      { name: 'Australia', code: 'AU' },
+      { name: 'Brazil', code: 'BR' },
+      { name: 'China', code: 'CN' },
+      { name: 'Egypt', code: 'EG' },
+      { name: 'France', code: 'FR' },
+      { name: 'Germany', code: 'DE' },
+      { name: 'India', code: 'IN' },
+      { name: 'Japan', code: 'JP' },
+      { name: 'Spain', code: 'ES' },
+      { name: 'United States', code: 'US' },
+    ],
+    events: [
+      { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0'},
+      { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+      { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
+      { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
+    ],
+  },
+  methods: {
+    onClick() {
+      this.errorToast = !this.errorToast
+    },
+    openBasic: function(){
+      this.displayBasic = true
+    },
+    hideSidebar(){
+      console.log(`click`)
+      this.visibleLeft = false
+    }
   },
   components: {
     'p-calendar': calendar,
     'p-checkbox': checkbox,
     'p-dropdown': dropdown,
     'p-inputtext': inputtext,
-    'radioinput': radiobutton,
+    radioinput: radiobutton,
     'p-button': button,
     'p-inputswitch': inputswitch,
-},
-watch:{
-  calendar(){
-    console.log(this.calendar)
-  }
-}
- // template: `<p>Hello</p>`,
- 
+    message: message,
+    'p-timeline': timeline,
+    card: card,
+    divider: divider,
+    'p-dialog': dialog,
+    'p-sidebar': sidebar,
+  },
+  watch: {
+    calendar() {
+      console.log(this.calendar)
+    },
+  },
+  // template: `<p>Hello</p>`,
 })
 
-
-
-document.documentElement.style = `color-scheme:dark`;
-
-
+document.documentElement.style = `color-scheme:dark`
