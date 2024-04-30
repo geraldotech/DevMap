7990648f86954136121aaf3684e67fe307fa2953
gmap-nitro <geraldo.filho92@hotmail.com>
2024-04-29 22:11:17 -0300

    :pencil: :books: :zap: DOCS

--- JavaScript/assets/thread/FormData/formDataEvent/formDataEvent.js
+++ JavaScript/assets/thread/FormData/formDataEvent/formDataEvent.js
@@ -5,7 +5,7 @@ form.onsubmit = () => {
 
   const formData = new FormData(event.target) // always return a empty {}
   console.log(formData.get("user"))
-  
+
   const formObj = Object.fromEntries(formData)
   console.log(formObj)
 
@@ -18,6 +18,32 @@ form.onsubmit = () => {
 
 
   //  destructuring
-  const {user, pwd} = data
+  const { user, pwd } = data
   console.log(user, pwd)
+}
+
+
+function setSelectedOption(value) {
+  const selectCat = document.getElementById('selectCat');
+  const options = selectCat.options;
+
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === value) {
+      options[i].selected = true;
+      break;
+    }
+  }
+}
+
+// Set selected option to "Android"
+setSelectedOption("Music");
+
+/* =============================== */
+
+const test001 = document.getElementById('test001')
+test001.onsubmit = function (e) {
+  e.preventDefault()
+  const name = document.Formsman.yourname.value
+  const selectedOption = document.Formsman.escola.value
+  console.log(name, selectedOption)
 }
